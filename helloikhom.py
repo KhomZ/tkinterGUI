1adec6b82fabb15278a9791cb98f0db7475c961f
kyzen khom <ikhom@ArchBox.localdomain>
2021-04-01 10:21:48 +0545

    Initial commit

---
+++ helloikhom.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+
+# Creating a label widget
+myLabel1 = Label(root, text="Hello ikhomkodes!")
+myLabel2 = Label(root, text="I'm kyzen Khom")
+
+# Showing it onto the screen
+myLabel1.pack()
+myLabel2.pack()
+
+
+root.mainloop()
+
